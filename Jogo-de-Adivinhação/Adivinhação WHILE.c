08b958c97fbf91b68003a545efdc4d7dfcc10c5f
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2023-11-12 15:01:19 -0300

    Atualizações

--- Jogo-de-Adivinhação/Adivinhação WHILE.c
+++ Jogo-de-Adivinhação/Adivinhação WHILE.c
@@ -4,13 +4,19 @@ int main() {
 
 #define NUMERO_DE_TENTATIVA 3
 
-printf("************************************\n");
-printf("* Bem-vindo ao jogo de Adivinhacao *\n");
-printf("************************************\n");
+printf("\n\n");
+printf("          P  /_\\  P                             ");
+printf("         /_\\_|_|_/_\\                           ");
+printf("    n_n | ||.  .|| | n_n          Bem-Vindo ao   ");
+printf("    |_|_|nnnn  nnnn|_|_|      Jogo de Adivinhacao");
+printf("   |\" \"  |   |_|  |\"  \"  |                   ");
+printf("   |_____| ' _ '  |_____|                        ");
+printf("        \\___|_|___//                            ");
+printf("\n\n");
 
 
 int chute;
-int numerosorte = 5;
+int numerosorte = 10;
 int ganhou = 0;
 int tentativas = 1;
 
@@ -18,7 +24,7 @@ while (!ganhou) {
 
     printf("Qual e o seu chute ?", tentativas);
     scanf("%d.", &chute);
-    printf("Seu chute foi %d. \n", chute);
+    printf("Seu chute foi %d\n", chute);
 
         if (chute < 0) {
             printf("Nada de numeros negativos Ze Pagao\n");
@@ -41,4 +47,11 @@ while (!ganhou) {
             tentativas++;
         }
     }
+    double score = 1000;
+
+    int ponto = (chute - numerosorte) / 2.0;
+    score = score - ponto;
+
+    printf("Voce conseguiu fazer %f pontos seu ruim!\n", score);
+    printf("Obrigado por jogar!!");
 }
