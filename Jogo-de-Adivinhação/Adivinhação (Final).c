6140c665ed115a860addd33c28f76183e2cb91c5
Luis Gustavo <luis.gustavo.sene99@gmail.com>
2023-11-25 22:19:46 -0300

    Update Final
    Está finalizado e funfando 100% o jogo de adivinhação.

--- Jogo-de-Adivinhação/Adivinhação (Final).c
+++ Jogo-de-Adivinhação/Adivinhação (Final).c
@@ -7,32 +7,35 @@
 int main() {
 
     printf("\n\n");
-    printf("          P  /_\\  P                             ");
-    printf("         /_\\_|_|_/_\\                           ");
-    printf("    n_n | ||.  .|| | n_n          Bem-Vindo ao   ");
-    printf("    |_|_|nnnn  nnnn|_|_|      Jogo de Adivinhacao");
-    printf("   |\" \"  |   |_|  |\"  \"  |                   ");
-    printf("   |_____| ' _ '  |_____|                        ");
-    printf("        \\___|_|___//                            ");
+    printf("          P  /_\\  P                             \n");
+    printf("         /_\\_|_|_/_\\                           \n");
+    printf("    n_n | ||.  .|| | n_n          Bem-Vindo ao   \n");
+    printf("    |_|_|nnnn  nnnn|_|_|      Jogo de Adivinhacao\n");
+    printf("   |\" \"  |   |_|  |\"  \"  |                   \n");
+    printf("   |_____| ' _ '  |_____|                        \n");
+    printf("        \\___|_|___//                            \n");
     printf("\n\n");
 
+    // Declarando variaveis que serao usadas mais a frente.
+    int acertou;
+    int chute;
+    int nivel;
+    int totaltentativa;
+
+    // Definindo a quantidade pontos inicial.
+    double pontos = 1000;
+
+    // Gerando um numero da sorte aleatorio.
+    srand(time(0));
+    int numerosorte = rand() % 100;
+
+    // Escolhendo a dificuldade.
     printf("Qual nivel de dificuldade voce quer jogar\n");
     printf("Facil [1] Normal [2] Dificil [3]\n\n");
     printf("Escolhe logo: ");
 
-    int acertou;
-    int chute;
-    int tentativas = 1;
-    int totaltentativa;
-
-    int segundo = time(0);
-    srand(segundo);
-    int numerogrande = rand();
-    int numerosorte = numerogrande % 10;
-
-    int nivel;
-
     scanf("%d", &nivel);
+
         switch (nivel) {
             case 1:
                 totaltentativa = 20;
@@ -47,54 +50,56 @@ int main() {
                 printf("Nao sei para quer ir no dificil voce e ruim!!\n\n");
                 break;
         }
-
+        
+        //Loop principal do jogo.
         for(int i = 1; i <= totaltentativa; i++) {
             printf("Tentativa %d de %d\n", i, totaltentativa);
-            printf("Qual e o seu chute ?  ", tentativas);
-            scanf("%d.", &chute, "\n\n");
-            printf("Se chute foi %d\n\n", chute);
 
+            printf("Qual e o seu chute ?  ");
+            scanf("%d", &chute);
+
+            printf("Se chute foi %d: \n\n", chute);
+            
+            //Tratando os chutes de números negativos.
             if (chute < 0) {
                 printf("Nada de numeros negativos Ze Pagao\n\n");
                 i--;
                 continue;
             }
+            //Verifica se acertou, foi maior ou menor.
             acertou = chute == numerosorte;
-            int maior = chute > numerosorte;
 
             if (acertou) {
                 break;
             }
-            else if (maior) {
+            else if (chute > numerosorte) {
                 printf("Seu chute foi maior\n\n");
             } else {
                 printf("Seu chute foi menor\n\n");
             }
-                tentativas++;        
-        }
 
-        double pontos = 1000;
-        double perdidos = abs(chute - numerosorte) / 2;
-        pontos = pontos - perdidos;
+            double perdidos = abs(chute - numerosorte) / 2.0;
+            pontos = pontos - perdidos;
+        }
 
         if (acertou) {
             printf("Pelo menos no jogo voce conseguiu alguma coisa\n\n");
             printf("Voce fez %.2f pontos\n\n", pontos);
         } else {
             printf("Minha avo e cega de um olho e faz melhor que isso %.2f \n\n", pontos);
-            printf("     .-.     ");
-            printf("     |U|     ");
-            printf("     | |     ");
-            printf("     | |     ");            
-            printf("     | |     ");
-            printf("    _| |_    ");
-            printf("   | | | |-. ");
-            printf("  /|     ` | ");
-            printf(" | |       | ");
-            printf(" |         | ");
-            printf(" \'        / ");
-            printf("  |       |  ");
-            printf("  |       |  ");
+            printf("     .-.     \n");
+            printf("     |U|     \n");
+            printf("     | |     \n");
+            printf("     | |     \n");            
+            printf("     | |     \n");
+            printf("    _| |_    \n");
+            printf("   | | | |-. \n");
+            printf("  /|     ` | \n");
+            printf(" | |       | \n");
+            printf(" |         | \n");
+            printf(" \'        / \n");
+            printf("  |       |  \n");
+            printf("  |       |  \n");
         }
         printf ("Obrigado por jogar pato!!\n");
     }
